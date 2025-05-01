852ce25625e0eea4d55a8a8065042ab3a1534313
SUJIT <SUJITnagmode@gmail.com>
2025-05-01 09:15:16 +0000

    commit4

--- myfile.java
+++ myfile.java
@@ -2,6 +2,6 @@
 
 class myfile {
     public static void main(String[] args) {
-        System.out.println("WELCOME TO THE JENKINS, World!"); 
+        System.out.println("WELCOME to to  TO THE JENKINS, World!"); 
     }
 }
